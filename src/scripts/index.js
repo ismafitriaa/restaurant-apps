1224fad073ae8fdf90d7434c2c1cc0993ee94e42
ariqluth <ttzluthfi@gamil.com>
2023-05-29 04:13:02 +0700

    menampilkan homepage, detail fix 1

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,45 +1,23 @@
-import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.css';
-import '../styles/responsive.css';
+import 'regenerator-runtime';
+import 'lazysizes';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 
-import ('../DATA.json').then(({default: jsonData}) => {
-    console.log(jsonData)
-    let datas = jsonData['restaurants']
-    let dataPost = '';
-    datas.forEach(function(data) {
-        dataPost +=`
-        <div class="post-item">
-            <img class="post-item__thumbnail" src="${data['pictureId']}" alt="${data['name']}" title="${data['name']}">
-            <div class="post-city">${data['city']}</div>
-            <div class="post-item__content">
-                <p class="post-item__rating">
-                    Rating : 
-                    <a href="#" class="post-item__rating__value">${data['rating']}</a>
-                </p>
-                <h1 class="post-item__title"><a href="#">${data['name']}</a></h1>
-                <div class="post-item__description">${data['description'].slice(0, 150)}...</div>
-            </div>
-        </div>
-        `;
-    });
-    document.querySelector('.posts').innerHTML = dataPost;  
+import '../styles/style.scss';
+import './view/components';
+import App from './view/app';
+import {swRegister} from './helper';
+import {getElement} from './helper';
+
+const app = new App({
+  appBar: getElement('app-bar'),
+  contentContainer: getElement('#content'),
 });
 
-// Menu
-const menu = document.querySelector('#menu');
-const hero = document.querySelector('.hero');
-const main = document.querySelector('main');
-const drawer = document.querySelector('#drawer');
-
-menu.addEventListener('click', function (event) {
-    drawer.classList.toggle('open');
-    event.stopPropagation();
+window.addEventListener('load', () => {
+  app.renderContent();
+  swRegister();
 });
 
-hero.addEventListener('click', function () {
-    drawer.classList.remove('open');
-});
-
-main.addEventListener('click', function () {
-    drawer.classList.remove('open');
+window.addEventListener('hashchange', () => {
+  app.renderContent();
 });
